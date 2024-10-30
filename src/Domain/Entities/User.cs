355f211ee1a0952dcdba100e4741679ad05c83a5
¨FranciscoCarballo01¨ <francisco.carballo.fc@gmail.com>
2024-10-30 12:13:30 -0300

    User correccion

--- src/Domain/Entities/User.cs
+++ src/Domain/Entities/User.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using Domain.Enums;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    internal class User
+    public abstract class User
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? Email { get; set; }
+        public RolEnum Rol { get; set; }
     }
 }

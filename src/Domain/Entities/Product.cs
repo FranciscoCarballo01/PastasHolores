cf019d4cae46bcdc1f68c294e3b027bcbd510d6a
mhonores02 <mhonores02@gmail.com>
2024-10-30 15:10:48 -0300

    Product

---
+++ src/Domain/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public float Price { get; set; }
+        public int Units { get; set; } = 1;
+        public string? Description { get; set; }
+        public string? Category { get; set; }
+    }
+}
